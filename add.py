62d90f061e3bd3b39222186ceeda799b2f27ed33
Vaishnavi Patil <vaishkpatil1217@gmail.com>
2025-12-10 12:21:09 +0530

    add,remove,discard,create

---
+++ add.py
@@ -0,0 +1,11 @@
+x={'apple', 'orange', 'mango', 'banana'}
+print("after creating set:",x)
+
+x.add("grape")
+print("after adding 'grapes':",x)
+
+x.remove("banana")
+print("after removing 'banana':",x)
+
+x.discard("mango")
+print("after discarding 'mango':",x)
