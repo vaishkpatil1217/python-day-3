516be387be7e07e257d02acd2f361e28c4a85fe9
Vaishnavi Patil <vaishkpatil1217@gmail.com>
2025-12-10 12:41:34 +0530

    intersectin set

---
+++ intersection.py
@@ -0,0 +1,11 @@
+x = {10, 20, 30, 40, 50}
+y = {30, 40, 50, 60, 70}
+
+print(x)
+print(y)
+
+z=x&y                    #(method 2)
+print("intersection set:",z)
+
+z=x.intersection(y)     #(method2)
+print("intersection set:",z)
