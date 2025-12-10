d1aca65c53d0699b00fe8d23d4978b170f7d65ce
Vaishnavi Patil <vaishkpatil1217@gmail.com>
2025-12-10 12:55:55 +0530

    difference of (x&y)

---
+++ difference.py
@@ -0,0 +1,16 @@
+x = {10, 20, 30, 40, 50}
+y = {30, 40, 50, 60, 70}
+print(x)
+print(y)
+
+z=x.difference(y)               #difference of x (method 1)
+print("difference of x",z)
+
+z=x.difference(y)               #difference of x (method 2)
+print("difference of x",z)
+
+z=y.difference(x)               #difference of y (method 1)
+print("difference of y",z)
+
+z=y.difference(x)               #difference of y (method 2)
+print("difference of y",z)
