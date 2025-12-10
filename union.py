a969cbbd61c6dca42e2b4414434bd0a88e4f76a8
Vaishnavi Patil <vaishkpatil1217@gmail.com>
2025-12-10 14:10:13 +0530

    union set

--- union.py
+++ union.py
@@ -1,7 +1,12 @@
 x = {10, 20, 30, 40, 50}
 y = {30, 40, 50, 60, 70}
 
-print(x)
+print(x)    #union set (method 1)
 print(y)
 z=x|y
 print("union set:",z)
+
+z=x.union(y)       # (method 2)
+print("union set:",z)
+
+
